7e22cef37b6b55b57c6d0f7e8f9b321117cdec4b
James Beard <jbeard@Jamess-MacBook-Pro.local>
2023-06-14 09:39:41 -0600

    basic project setup

---
+++ e2e/constants.js
@@ -0,0 +1,20 @@
+module.exports = {
+    /* all locator strategies:
+            css selector
+            link text
+            partial link text
+            tag name
+            xpath
+       https://nightwatchjs.org/api/commands/#element
+    */
+    SELECTORS: {
+        CSS: 'css selector',
+        XPATH: 'xpath',
+        TAGNAME: 'tag name'
+    },
+    PAUSE: {
+        pauseFor1: '1000',
+        pauseFor2: '2000',
+        pauseFor3: '3000'
+    },
+};
