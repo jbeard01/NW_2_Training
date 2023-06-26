5117c6adcb8b5db4943f24fe16f11f53d85ddf70
JBeard01 <88330651+jbeard01@users.noreply.github.com>
2023-06-26 08:14:19 -0600

    Update buttonTests.js

--- e2e/test-scripts/buttonTests.js
+++ e2e/test-scripts/buttonTests.js
@@ -5,35 +5,42 @@ module.exports = {
     '@tags': ['null'], // tags to target for run script
 
     /*
-    Module 2
-    The objective of this module is to learn about and understand the POM or Page Object Model, hard vs soft assertions, and how to call a page object function inside a test script. 
-    In this exercise, code will be moved to a page object file, and a page object function created to be used in it's place. 
-    This will highlight how the POM works, and assert vs verify.
+    Module 3 (part 1)
+    The objective of this module is to learn about hardcoding, data abstraction, before/after hooks, and extended Page Object Model methods.
+    Using the provided constants file, abstract hardcoded data from the tests into the constants file. 
+        
+    Exercise 1 Buttons: 
+        1) Rename the test suite file to better represent the testing being conducted (buttonTests.js)
+        2) Rename the page object page for exercise 1 to better represent the functions used for testing (buttonPage.js)
+            *** Note: you must also update the function calls to match the new naming for the page object page
+        3) Using data abstraction, abstract hardcoded data points into the constants file for use in the tests
+            Example: line 32  browser.click(constants.SELECTORS.CSS, '[id="checkbox2"]');
+                     line 32  browser.click(constants.SELECTORS.CSS, constants.BUTTONS.chkBox2);
 
-    Using the provided example, create a Page Object Function that will complete the test. 
-        *** NOTE: Page Object Functions (PO Function) should not contain hard assertions! ***
+    Module 3 (part 2)
+    In part 2, create a new page object (navPage.js) with common navigation functions
+        1) Using data abstraction, move page specific elements to the elements object of the buttonPage.js
+        2) Identify commonly used navigation functions from all 3 test suites
+            1) Move these functions to the navPage.js file
+            2) Create a function to navigate to a menu item, and if applicable it's sub-menu items, by data passed into the function
+                a) navToMenuItem
+            3) Remove redundant navigation functions
+            4) Update navigation functions in buttonTests.js
+        3) Add beforeEach and afterEach hooks to the test suite (these go above the test scripts)
+            1) beforeEach
+                a) navToSite
+            2) afterEach
+                a) browser.end();
 
-        Exercise 1: Buttons (modify existing long form scripts)
+        Example:
 
-            Script: Checkboxes page object function
-                Create a page object function (verifyChkBoxes) that does the following:
-                    1) Navigate to the buttons menu and select Checkboxes
-                    2) Verify the landing page URL is correct
-                    3) Verify the 3 checkboxes are present
-
-                1) Click the 2nd checkbox
-                2) Reset the selection
-            
-            Script: Radio buttons page object function
-                Create a page object function (verifyRadioBtns) that does the following:
-                    1) Navigate to the buttons menu and select Radio buttons
-                    2) Verify the 4 radio button options are present
-                    3)Verify the landing page URL is correct 
-                1) Click the first radio button
-                2) Return to the home screen
-                    - BONUS: Create a page object function (navToHome) to use then navbar to return home, verify URL
-                    1) Use the navbar functions of the website to navigate to the home screen
-                    2) Verify the home page by URL
+            beforeEach: function(browser){
+                const nav = browser.page.navPage();
+                nav.navToSite();
+            },
+            afterEach: function(browser){
+                browser.end();
+            },
     */
 
     beforeEach: function(browser){
